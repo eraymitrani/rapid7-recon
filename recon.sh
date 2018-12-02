e9796b2bafaa5ffb8b79d83e2517f841cceee6cb
Eray Mitrani <eraymitrani@gmail.com>
2018-12-01 22:03:29 -0500

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,4 +1,4 @@
 cat td.txt | grep "\.$1.com\"" > $1.txt
 python parse.py $1.txt > result.txt
 sleep 1
-wfuzz -z file,result.txt -Z -t 100 --filter "c=200" -H 'X-Forwarded-For: 127.0.0.1, localhost' https://FUZZ.com/$2
+wfuzz -z file,result.txt -Z -t 100 --filter "c=200" -H 'X-Forwarded-For: 127.0.0.1, localhost' https://FUZZ/$2
